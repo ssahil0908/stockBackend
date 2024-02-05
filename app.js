ee7846435c930e8256872cad0cbfa78596a6659b
sahil <sahil@growbizsolutions.com>
2024-02-05 22:31:34 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const dotenv=require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 5000;
 app.use(express.json());
 const mongoose = require('./config/db');
 
